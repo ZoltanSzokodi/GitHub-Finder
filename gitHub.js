0097a1a6ee39f7d87377b55f6b5baaa4fc72fd73
Rupert <zoltan.szokodi@hotmail.com>
2019-05-20 16:54:52 +0200

    Fetch & Display Repos

--- gitHub.js
+++ gitHub.js
@@ -2,15 +2,21 @@ class Github {
     constructor() {
         this.client_id = 'e8e4c32f1a3ffe9904f2',
         this.client_secret = '2414e83384688416783c17a75d4f3fc25a9d01bf'
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
